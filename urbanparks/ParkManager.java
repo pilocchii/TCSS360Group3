3b17b8e3a6fa072ed713f2df37e748832f29b723
Aashish Kumar <vats96ashu@users.noreply.github.com>
2018-02-11 23:50:44 -0800

    Update ParkManager.java
    fixed all 3 rules and exceptions

--- urbanparks/ParkManager.java
+++ urbanparks/ParkManager.java
@@ -4,6 +4,7 @@ package urbanparks;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 
 /**
@@ -13,46 +14,92 @@ import java.util.Calendar;
  */
 public class ParkManager extends User {
 
-	/** The park name. */
-	private String myParkName;
-	
-	/** The park zipCode. */
-	private int myParkZipCode;
-	
+
 	private final int MAXIMUM_NUMBER_OF_DAYS = 3;
-	
+
 	private final int MAXIMUM_NUMBER_OF_PENDING_JOBS = 20	;
-	
+
 	private final int MAXIMUM_NUMBER_OF_JOB_END_DAYS = 75;
-	
+
 	private ArrayList<Job> jobsCreated;
+
+	private HashMap<Integer, Job> jobsList;
+
+	public JobCollection collectionJobs;
+
+
+
+
+
 
 	public ParkManager(String theFirstName, String theLastName, String theEmail, String thePhone) {
 		super(theFirstName, theLastName, theEmail, thePhone);
+		jobsList  = collectionJobs.getJobCollection();
 
 	}
-	
-	public ParkManager(String theFirstName, String theLastName, String theEmail, String thePhone, String theParkName, int theParkZipCode) {
-		super(theFirstName, theLastName, theEmail, thePhone);
-		setParkName(theParkName);
-		setParkZipCode(theParkZipCode);
-		
+
+	public class mangerPendingJobsException extends Exception {
+		public mangerPendingJobsException() {}
+		public mangerPendingJobsException(String message) {
+			super(message);
+		}
 	}
-	
+	public class managerJobDaysException extends Exception {
+		public managerJobDaysException() {}
+		public managerJobDaysException(String message) {
+			super(message);
+		}
+	}
+
+	public class managerJobEndDaysException extends Exception {
+		public managerJobEndDaysException() {}
+		public managerJobEndDaysException(String message) {
+			super(message);
+		}
+	}
+
 	/**
 	 * Checks if a job can be created or not.
 	 * 
 	 * @param candidateJob
 	 * @return list of jobs created;
 	 */
-	public ArrayList<Job> createdANewJob(Job candidateJob) {
-		if(isMaximumJobDays(candidateJob) && isMaximumEndDays(candidateJob)) {
-			jobsCreated.add(candidateJob);
+	public ArrayList<Job> createdANewJob(Job candidateJob) throws mangerPendingJobsException, 
+	managerJobDaysException, managerJobEndDaysException {
+		if(isMaximumJobDays(candidateJob))  {
+			throw new managerJobDaysException();
+
 		}
+		if(isMaximumEndDays(candidateJob)) {
+			throw new managerJobEndDaysException();
+
+		}
+		if(isPendingJobs()) {
+			throw new mangerPendingJobsException();
+		}
+		jobsCreated.add(candidateJob);
+
 		return jobsCreated;
-		
+
 	}
-	
+	public boolean isPendingJobs() {
+		int counter = 0;
+		boolean pending = false;
+		for(Integer jobId: jobsList.keySet()) {
+			Job currentJob = jobsList.get(jobId);
+			Calendar currentDate = Calendar.getInstance();		
+			if(compare2Date(currentDate, currentJob.getEndDateTime()) < 0) {
+				counter++;
+			}
+		}
+		if(counter < MAXIMUM_NUMBER_OF_PENDING_JOBS) {
+			pending = true;
+
+		}
+		return pending;
+	}
+
+
 	/**
 	 * Check that job cann't be specified if it takes more than the maximum number of days
 	 * 
@@ -63,11 +110,11 @@ public class ParkManager extends User {
 
 		Calendar maximumDate = Calendar.getInstance();
 		maximumDate.add(Calendar.DAY_OF_MONTH, MAXIMUM_NUMBER_OF_DAYS);
-		
 
-		return compare2Date(theCondidateJob.getStartDateTime(), maximumDate) >= 0;
+
+		return compare2Date(theCondidateJob.getStartDateTime(), maximumDate) <=0;
 	}
-	
+
 	/**
 	 * Check that job cann't be specified whose end date is more than the maximum number of days
 	 * 
@@ -78,14 +125,14 @@ public class ParkManager extends User {
 
 		Calendar maximumStartDate = Calendar.getInstance();
 		maximumStartDate.add(Calendar.DAY_OF_MONTH, MAXIMUM_NUMBER_OF_JOB_END_DAYS);
-		
 
-		return compare2Date(theCondidateJob.getEndDateTime(), maximumStartDate) >= 0;
+
+		return compare2Date(theCondidateJob.getEndDateTime(), maximumStartDate) <=0;
 	}
-	
-	
-	////public boolean pendingJobs();
-	
+
+
+
+
 	/**
 	 * Comparing two dates by Year, Month and day.
 	 * 
@@ -112,20 +159,8 @@ public class ParkManager extends User {
 
 	}
 
-	public String getParkName() {
-		return myParkName;
-	}
 
-	public void setParkName(String myParkName) {
-		this.myParkName = myParkName;
-	}
 
-	public int getParkZipCode() {
-		return myParkZipCode;
-	}
-
-	public void setParkZipCode(int myParkZipCode) {
-		this.myParkZipCode = myParkZipCode;
-	}
 
 }
+
