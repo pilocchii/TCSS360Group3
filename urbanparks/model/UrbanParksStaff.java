a0b0b45d5a8682e1876e971018e36decc2f77763
Aashish Kumar <vats96ashu@users.noreply.github.com>
2018-03-04 20:29:16 -0800

    Update UrbanParksStaff.java

--- urbanparks/model/UrbanParksStaff.java
+++ urbanparks/model/UrbanParksStaff.java
@@ -4,6 +4,9 @@ public class UrbanParksStaff extends User {
 
 	private static final long serialVersionUID = 3702035140593797417L;
 	
+	/**
+	 * Default constructor for Urban park staff.
+	 */
 	public UrbanParksStaff(String theFirstName, String theLastName, String theEmail, String thePhone) {
 		super(theFirstName, theLastName, theEmail, thePhone);
 	}
