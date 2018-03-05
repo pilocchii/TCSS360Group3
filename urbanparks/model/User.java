feb431e5e0fce1de2bab031d1e050bfeda6377e9
Aashish Kumar <vats96ashu@users.noreply.github.com>
2018-03-04 19:28:21 -0800

    Update User.java

--- urbanparks/model/User.java
+++ urbanparks/model/User.java
@@ -10,9 +10,16 @@ public abstract class User implements Serializable {
 
 	private static final long serialVersionUID = -4751543109134325926L;
 	
+	/** First name of user*/
 	private String firstName;
+	
+	/** Last name of user*/
 	private String lastName;
+	
+	/** Email of user for log in*/
 	private String email;
+	
+	/** Phone number of user*/
 	private String phoneNum;
 
 	/** List of all jobs that user accepted or created. */
@@ -41,6 +48,7 @@ public abstract class User implements Serializable {
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	/**
 	 * Return the last name of the user.
 	 * @return the last name.
@@ -64,7 +72,10 @@ public abstract class User implements Serializable {
 		return phoneNum;
 	}
 	
-	
+	/**
+	 * Match the Job Id in with jobs in collections.
+	 * @return true if job id matches job in jobs collections.
+	 */
 	public boolean isAssociatedWithJob(Job job) {
 		for (long jobID : associatedJobs) {
 			if (jobID == job.getJobId()) {
