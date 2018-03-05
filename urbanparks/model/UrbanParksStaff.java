c74213f95be2ecf0351413606fd4512b20dd814b
Aashish Kumar <vats96ashu@users.noreply.github.com>
2018-03-04 20:43:50 -0800

    Update UrbanParksStaff.java

--- urbanparks/model/UrbanParksStaff.java
+++ urbanparks/model/UrbanParksStaff.java
@@ -7,10 +7,10 @@ public class UrbanParksStaff extends User {
 	/**
 	 * Default constructor for Urban park staff.
 	 * 
-	 * @param firstName the first name of the user.
-	 * @param lastName the last name of the user.
-	 * @param email the email address of the user.
-	 * @param phone the phone number of the user.
+	 * @param theFirstName the first name of the user.
+	 * @param theLastName the last name of the user.
+	 * @param theEmail the email address of the user.
+	 * @param thePhone the phone number of the user.
 	 */
 	public UrbanParksStaff(String theFirstName, String theLastName, String theEmail, String thePhone) {
 		super(theFirstName, theLastName, theEmail, thePhone);
